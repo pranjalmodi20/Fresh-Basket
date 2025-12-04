e9a245bbdfba2c23e792a787b6dd6458a096c32f
pranjalmodi20 <pranjalmodibkn@gmail.com>
2025-12-04 17:08:49 +0530

    updated

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const router = express.Router();
 const { signup, login } = require('../controllers/authController');
-const User = require('../models/User');
+const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/login', login);
